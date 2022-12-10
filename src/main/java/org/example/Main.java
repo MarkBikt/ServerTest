7663c6e0682df1ca1ee6606de5b0591b0fa5e18c
Mark <mark.biktiryakov@rambler.ru>
2022-12-10 13:19:22 +0300

    changeNew

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AllRequestsServlet frontend = new AllRequestsServlet();
+
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(frontend), "/mirror");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        try {
+            server.start();
+            java.util.logging.Logger.getGlobal().info("Server started");
+            server.join();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
