8c4a7ba0b9bffcca0d719fcebab5c6654c068fdd
Mark <mark.biktiryakov@rambler.ru>
2022-12-12 11:56:45 +0300

    main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) {
         AllRequestsServlet frontend = new AllRequestsServlet();
 
-
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(frontend), "/mirror");
 
